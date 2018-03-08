ff615833f78f4fc7b5ef5251e314b137ffbe12f1
Ryan Butler <ryanbutler@users.noreply.github.com>
2018-03-08 09:33:33 -0600

    initial commit.

---
+++ MvcContactForm/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using MvcContactForm.Models;
+using System.Text;
+
+namespace MvcContactForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModels c)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.Email.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.To.Add("youremail@email.com");
+                    msg.Subject = "Contact Us";
+                    msg.IsBodyHtml = false;
+                    smtp.Host = "mail.yourdomain.com";
+                    smtp.Port = 25;
+                    sb.Append("First name: " + c.FirstName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Last name: " + c.LastName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Email: " + c.Email);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Comments: " + c.Comment);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+            }
+            return View();
+        }
+           
+        }
+    }
---
+++ MvcContactForm/Models/ContactModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcContactForm.Models
+{
+    public class ContactModels
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+        public string Email{ get; set; }
+        public string Comment{ get; set; }
+    }
+}
